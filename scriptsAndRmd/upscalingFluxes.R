7bcec107bf64e93e1bfe9a2001416b23da9e3a49
Waldo <waldo.sarah@epa.gov>
2019-04-07 15:57:00 -0400

    changed the upscaling of errors

--- scriptsAndRmd/upscalingFluxes.R
+++ scriptsAndRmd/upscalingFluxes.R
@@ -15,7 +15,7 @@
     # LPCR1169, SJR Confluence, Oak Canyon, Crossing of the Fathers, Navajo, Camp 1, Wahweap
 
 ###If starting with this code:
-lakePowellMeans2<-read.csv(paste(myWD, "output/lakePowellFluxes20190322.csv", sep="/"))
+lakePowellMeans<-read.csv(paste(myWD, "output/lakePowellFluxes20190322.csv", sep="/"))
 
 ##############Mean diffusive rate for tributary areas: -----
 
@@ -29,40 +29,58 @@ ggplot(LP.littoral, aes(CH4trate.mean))+
 
 LP.littoral.summary<-LP.littoral%>%
   summarize(meanCH4lit = mean(CH4trate.mean),
-            sdCH4lit = exp(sd(log(CH4trate.mean))), #log is the natural log transformation, since the msmts are log-normally distributed
-            seCH4lit = sdCH4lit/8,
-            fracErrlit = seCH4lit/meanCH4lit*100)
+            sdCH4lit = sd(log(CH4trate.mean)), #log is the natural log transformation, since the msmts are log-normally distributed
+            nCH4lit = n(),
+            seCH4lit = sdCH4lit/sqrt(nCH4lit),
+            CH4lower = exp(log(meanCH4lit)-seCH4lit),
+            CH4upper = exp(log(meanCH4lit)+seCH4lit),
+            fracErrlit.L = abs(meanCH4lit-CH4lower)/meanCH4lit*100,
+            fracErrlit.U = abs(meanCH4lit-CH4upper)/meanCH4lit*100)
+            
 meanCH4lit<-mean(LP.littoral$CH4trate.mean) #mean CH4 emission rate from littoral sites in mg CH4 m-2 hr-1
 #convert from mg CH4 to kg CH4; m2 to km2; then upscaling to total littoral area (33.2 km2); convert from hr-1 to y-1
 totCH4.lit.kg.y<-meanCH4lit/10^6*1000^2*33.2*24*365 #units: kg CH4 y-1 
-ErrCH4.lit.kg.y<-LP.littoral.summary$fracErrlit/100*totCH4.lit.kg.y
+ErrCH4L.lit.kg.y<-LP.littoral.summary$fracErrlit.L/100*totCH4.lit.kg.y
+ErrCH4U.lit.kg.y<-LP.littoral.summary$fracErrlit.U/100*totCH4.lit.kg.y
+
 
 LP.openWater<-filter(lakePowellMeans, site!="Colorado Inlet",
                      site != "Sheep Canyon",
                      site != "Camp 3", site != "Hite Canyon", site != "SJR Inlet",
                      site != "Alcove", site != "Escalante Inflow", site != "Garces Island")
 summary(LP.openWater)
+ggplot(LP.openWater, aes(log(CH4trate.mean)))+
+  geom_histogram(bins=20)
+
 LP.openWater.summary<-LP.openWater%>%
   summarize(meanCH4ow = mean(CH4trate.mean),
-            sdCH4ow = exp(sd(log(CH4trate.mean))),
-            seCH4ow = sdCH4ow/20,
-            fracErrow = seCH4ow/meanCH4ow*100)
+            sdCH4ow = sd(log(CH4trate.mean)), #log is the natural log transformation, since the msmts are log-normally distributed
+            nCH4ow = n(),
+            seCH4ow = sdCH4ow/sqrt(nCH4ow),
+            CH4lower = exp(log(meanCH4ow)-seCH4ow),
+            CH4upper = exp(log(meanCH4ow)+seCH4ow),
+            fracErrOw.L = abs(meanCH4ow-CH4lower)/meanCH4ow*100,
+            fracErrOw.U = abs(meanCH4ow-CH4upper)/meanCH4ow*100)
 
 meanCH4ow<-mean(LP.openWater$CH4trate.mean) #mean CH4 emission rate from littoral sites in mg CH4 m-2 hr-1
 #convert from mg CH4 to kg CH4; m2 to km2; then upscaling to total littoral area (33.2 km2); convert from hr-1 to y-1
 totCH4.ow.kg.y<-meanCH4ow/10^6*1000^2*(467-33.2)*24*365 #units: kg CH4 y-1 
-ErrCH4.ow.kg.y<-LP.openWater.summary$fracErrlit/100*totCH4.ow.kg.y
+ErrCH4L.ow.kg.y<-LP.openWater.summary$fracErrOw.L/100*totCH4.ow.kg.y
+ErrCH4U.ow.kg.y<-LP.openWater.summary$fracErrOw.U/100*totCH4.ow.kg.y
+
 
 totCH4.powell.kg.y<-sum(totCH4.lit.kg.y, totCH4.ow.kg.y)
-totCH4err.powell.kg.y<-sqrt(sum(ErrCH4.lit.kg.y^2, ErrCH4.ow.kg.y^2))
+totCH4errL.powell.kg.y<-sqrt(sum(ErrCH4L.lit.kg.y^2, ErrCH4L.ow.kg.y^2))
+totCH4errU.powell.kg.y<-sqrt(sum(ErrCH4U.lit.kg.y^2, ErrCH4U.ow.kg.y^2))
 
-totFracErr<-totCH4err.powell.kg.y/totCH4.powell.kg.y*100 #as a percent, 6.5%
+totFracErrL<-totCH4errL.powell.kg.y/totCH4.powell.kg.y*100 #as a percent, 42%
+totFracErrU<-totCH4errU.powell.kg.y/totCH4.powell.kg.y*100 #as a percent, 79%
 
 totCH4.powell.g.m2.y<-totCH4.powell.kg.y/(467*10^6)*1000 #units: g CH4 m-2 y-1
 powell.areal.ch4.co2e<-totCH4.powell.g.m2.y*34 #converting from gCH4 to CO2e, for units of g CO2e m-2 y-1
 powell.areal.ch4.co2e #239.5
-powell.areal.ch4.co2e.err<-powell.areal.ch4.co2e*totFracErr/100 #+/- 15.5
-
+powell.areal.ch4.co2e.L<-powell.areal.ch4.co2e-(powell.areal.ch4.co2e*totFracErrL/100) #lower range: 137.6
+powell.areal.ch4.co2e.U<-powell.areal.ch4.co2e+(powell.areal.ch4.co2e*totFracErrU/100) #upper range: 429.75
 
 ###############CO2##################################
 
@@ -73,19 +91,26 @@ ggplot(lakePowellMeans, aes(CO2trate.mean))+
 LP.CO2.summary<-lakePowellMeans%>%
   summarize(meanCO2 = mean(CO2trate.mean, na.rm=TRUE), #mg CO2 m-2 hr-1
             sdCO2 = (sd(CO2trate.mean, na.rm=TRUE)), #no log transformation because 1) co2 fluxes are almost normally distributed and 2) negative values
-            seCO2 = sdCO2/30,
+            seCO2 = sdCO2/sqrt(30),
             fracErrCO2 = seCO2/meanCO2*100)
 
 powell.areal.CO2.g.m2.y<-LP.CO2.summary$meanCO2*24*365/1000
 powell.areal.CO2.g.m2.y #85.45 g CO2-e m-2 y-1
 powell.areal.CO2err.g.m2.y<-totCO2.g.m2.y*LP.CO2.summary$fracErrCO2/100
-powell.areal.CO2err.g.m2.y #+/- 8.7
+powell.areal.CO2err.g.m2.y #+/- 47.8
+powell.areal.CO2.L<-powell.areal.CO2.g.m2.y-powell.areal.CO2err.g.m2.y #37.6
+powell.areal.CO2.U<-powell.areal.CO2.g.m2.y+powell.areal.CO2err.g.m2.y #133.2
+
+
 
 totalArealEmissions<-sum(totCO2.g.m2.y, powell.areal.ch4.co2e)
 totalArealEmissions #324.95
-totArealError<-sqrt(sum(powell.areal.ch4.co2e.err^2, totCO2err.g.m2.y^2))
-totArealError #+/- 17.7
-
+totArealErrorL<-sqrt(sum((powell.areal.ch4.co2e*totFracErrL/100)^2, powell.areal.CO2err.g.m2.y^2))
+totFracErrL<-totArealErrorL/totalArealEmissions*100 #34%
+totArealErrorU<-sqrt(sum((powell.areal.ch4.co2e*totFracErrU/100)^2, powell.areal.CO2err.g.m2.y^2))
+totFracErrU<-totArealErrorU/totalArealEmissions*100 #60%
+totArealCO2eL<-totalArealEmissions-totArealErrorL #212.4
+totArealCO2eU<-totalArealEmissions+totArealErrorU #521.1
 #####In units of kg CO2-e MWh-1
 #converting from g CO2-e m-2 y-1 to kg CO2e MWh-1
 # power production number: 5,196,113 MWh y-1, based on avg generation from 1978 - 1999
@@ -97,17 +122,21 @@ area_elec<-areaPowell*10^6/hydroPower #m2 y per MWh
 
 powell.ef.ch4.co2e<-powell.areal.ch4.co2e*area_elec/1000 #kg CO2-e MWh-1
 powell.ef.ch4.co2e #21.52
-powell.ef.err.ch4.co2e<-powell.ef.ch4.co2e*totFracErr/100
-powell.ef.err.ch4.co2e #+/- 1.4
+powell.ef.ch4.co2eL<-powell.ef.ch4.co2e-(powell.ef.ch4.co2e*totFracErrL/100)
+powell.ef.ch4.co2eU<-powell.ef.ch4.co2e+(powell.ef.ch4.co2e*totFracErrU/100)
+
 
 powell.ef.co2.co2e<-powell.areal.CO2.g.m2.y*area_elec/1000
 powell.ef.co2.co2e #7.68
 powell.ef.err.co2.co2e<-powell.ef.co2.co2e*LP.CO2.summary$fracErrCO2/100
 powell.ef.err.co2.co2e
+powell.ef.co2.co2e.L<-powell.ef.co2.co2e-powell.ef.err.co2.co2e
+powell.ef.co2.co2e.U<-powell.ef.co2.co2e+powell.ef.err.co2.co2e
 
 totEF.emissions<-sum(powell.ef.ch4.co2e, powell.ef.co2.co2e)
 totEF.emissions #29.2
-totEF.err<-sqrt(sum(powell.ef.err.co2.co2e^2, powell.ef.err.ch4.co2e^2))
+totEF.errL<-totEF.emissions-sqrt(sum((powell.ef.ch4.co2e*totFracErrL/100)^2, powell.ef.err.co2.co2e^2))
+totEF.errU<-totEF.emissions+sqrt(sum((powell.ef.ch4.co2e*totFracErrU/100)^2, powell.ef.err.co2.co2e^2))
 
 # filter(lakePowellData10, site=="Colorado Inlet" | site == "Sheep Canyon" | site == "Camp 3" | site == "Hite Canyon"| site == "SJR Inlet"|site == "Alcove" | site == "Escalante Inflow" | site == "Garces Island") %>%
 #   summarize(mean.ch4 = mean(ch4.drate.mg.h, na.rm=TRUE),
