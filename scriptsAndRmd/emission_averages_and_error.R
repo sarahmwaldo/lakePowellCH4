e5b7a0bf0fedc3c5de721eeeb10f4a395c7d4321
Deemer <bdeemer@usgs.gov>
2019-05-19 13:12:28 -0700

    new script looking at relationship between reservoir elevation and per MWh emission

--- scriptsAndRmd/emission_averages_and_error.R
+++ scriptsAndRmd/emission_averages_and_error.R
@@ -14,63 +14,82 @@ master$region<-ifelse(master$site %in% Littoral_Tributary,"tributary","main")
 # emission rates calculated starting on line 190 of calculate emissions lp10 script
 summary_rates<-master %>%
   group_by(region)%>%
-  summarise(CH4trate.mean=mean(CH4trate.mean,na.rm=TRUE)*24,
-            CO2trate.mean=mean(CO2trate.mean,na.rm=TRUE)*24,
-            meanCH4.funnel=mean(meanCH4.funnel,na.rm=TRUE)*24,
-            CH4drate.mean=mean(CH4drate.mean,na.rm=TRUE)*24,
-            CH4erate.mean=mean(CH4erate.mean,na.rm=TRUE)*24,
-            CO2trate.mean=mean(CO2trate.mean,na.rm=TRUE)*24)
+  summarise(CH4trate.mean2=mean(CH4trate.mean,na.rm=TRUE)*24,
+            CO2trate.mean2=mean(CO2trate.mean,na.rm=TRUE)*24,
+            meanCH4.funnel2=mean(meanCH4.funnel,na.rm=TRUE)*24,
+            CH4drate.mean2=mean(CH4drate.mean,na.rm=TRUE)*24,
+            CH4erate.mean2=mean(CH4erate.mean,na.rm=TRUE)*24,
+            CH4trate.na=(sum(is.na(CH4trate.mean))),
+            CH4trate.sd=sd(CH4trate.mean,na.rm=T),
+            CO2trate.na=(sum(is.na(CO2trate.mean))),
+            CO2trate.sd=sd(CO2trate.mean,na.rm=T),
+            CH4drate.na=(sum(is.na(CH4drate.mean))),
+            CH4drate.sd=sd(CH4drate.mean,na.rm=T),
+            CH4erate.na=(sum(is.na(CH4erate.mean))),
+            CH4erate.sd=sd(CH4erate.mean,na.rm=T),
+            CH4funnel.na=(sum(is.na(meanCH4.funnel))),
+            CH4funnel.sd=sd(meanCH4.funnel,na.rm=T)
+            )
 
-summary_rate_error<- diffusive %>%
-  mutate(region=ifelse(site %in% Littoral_Tributary,"tributary","main"))%>%
-  mutate(CH4trate.log=log(CH4trate.mean))%>%
-  mutate(CH4drate.log=log(CH4drate.mean))%>%
-  mutate(CH4erate.log=log(CH4erate.mean+1))%>%
-  mutate(meanCH4.funnel.log=log(meanCH4.funnel+1))%>%
-  group_by(region)%>%
-  summarise(CH4trate.sd=sd(CH4trate.log,na.rm=T),
-            CH4trate.na=(sum(is.na(CH4trate.log))),
-            CH4trate.log=mean(CH4trate.log,na.rm=T),
+## I'm hashing out this code which was looking at log means-- you can use control shift C for that!
+#summary_rate_error<- master %>%
+  #mutate(CH4trate.log=log(CH4trate.mean))%>%
+  #mutate(CH4drate.log=log(CH4drate.mean))%>%
+  #mutate(CH4erate.log=log(CH4erate.mean+1))%>%
+  #mutate(CO2trate.log=log(CO2trate.mean+38))%>%
+  #mutate(meanCH4.funnel.log=log(meanCH4.funnel+1))%>%
+  #group_by(region)%>%
+  #summarise(CH4trate.sd=sd(CH4trate.log,na.rm=T),
+            #CH4trate.na=(sum(is.na(CH4trate.log))),
+            #CH4trate.log=mean(CH4trate.log,na.rm=T),
             
-            meanCH4.funnel.sd=sd(meanCH4.funnel.log,na.rm=T),
-            meanCH4.funnel.na=(sum(is.na(meanCH4.funnel.log))),
-            meanCH4.funnel.log=mean(meanCH4.funnel.log,na.rm=T),
+            #meanCH4.funnel.sd=sd(meanCH4.funnel.log,na.rm=T),
+            #meanCH4.funnel.na=(sum(is.na(meanCH4.funnel.log))),
+            #meanCH4.funnel.log=mean(meanCH4.funnel.log,na.rm=T),
             
-            CH4drate.sd=sd(CH4drate.log,na.rm=T),
-            CH4drate.na=(sum(is.na(CH4drate.log))),
-            CH4drate.log=mean(CH4drate.log,na.rm=T),
-            
-            CH4erate.sd=sd(CH4erate.log,na.rm=T),
-            CH4erate.na=(sum(is.na(CH4erate.log))),
-            CH4erate.log=mean(CH4erate.log,na.rm=T))
+            # CH4drate.sd=sd(CH4drate.log,na.rm=T),
+            # CH4drate.na=(sum(is.na(CH4drate.log))),
+            # CH4drate.log=mean(CH4drate.log,na.rm=T),
+            # 
+            # CH4erate.sd=sd(CH4erate.log,na.rm=T),
+            # CH4erate.na=(sum(is.na(CH4erate.log))),
+            # CH4erate.log=mean(CH4erate.log,na.rm=T),
+            # 
+            # CO2trate.sd=sd(CO2trate.log,na.rm=T),
+            # CO2trate.na=(sum(is.na(CO2trate.log))),
+            # CO2trate.log=mean(CO2trate.log,na.rm=T))
 
 #these tables are generating fluxes in units of per hour, so transform them here
-Error_tributary<-summary_rate_error %>%
-  filter(region=="tributary")%>%
-  mutate(CH4trate.se=CH4trate.sd/sqrt(8-CH4trate.na))%>%
-  mutate(CH4trate.upper=24*exp(CH4trate.se+CH4trate.log))%>%
-  mutate(CH4trate.lower=24*exp(CH4trate.log-CH4trate.se))%>%
-  mutate(meanCH4.funnel.se=meanCH4.funnel.sd/sqrt(8-meanCH4.funnel.na))%>%
-  mutate(meanCH4.funnel.upper=24*exp(meanCH4.funnel.log+meanCH4.funnel.se))%>%
-  mutate(meanCH4.funnel.lower=24*exp(meanCH4.funnel.log-meanCH4.funnel.se))%>%
-  mutate(CH4drate.se=CH4drate.sd/sqrt(8-CH4drate.na))%>%
-  mutate(CH4drate.upper=24*exp(CH4drate.log+CH4drate.se))%>%
-  mutate(CH4drate.lower=24*exp(CH4drate.log-CH4drate.se))%>%
-  mutate(CH4erate.se=CH4erate.sd/sqrt(8-CH4erate.na))%>%
-  mutate(CH4erate.upper=24*exp(CH4erate.log+CH4erate.se))%>%
-  mutate(CH4erate.lower=24*exp(CH4erate.log-CH4erate.se))
-
-Error_main<-summary_rate_error %>%
-  filter(region=="main")%>%
-  mutate(CH4trate.se=CH4trate.sd/sqrt(22-CH4trate.na))%>%
-  mutate(CH4trate.upper=24*exp(CH4trate.se+CH4trate.log))%>%
-  mutate(CH4trate.lower=24*exp(CH4trate.log-CH4trate.se))%>%
-  mutate(meanCH4.funnel.se=meanCH4.funnel.sd/sqrt(22-meanCH4.funnel.na))%>%
-  mutate(meanCH4.funnel.upper=24*exp(meanCH4.funnel.log+meanCH4.funnel.se))%>%
-  mutate(meanCH4.funnel.lower=24*exp(meanCH4.funnel.log-meanCH4.funnel.se))%>%
-  mutate(CH4drate.se=CH4drate.sd/sqrt(22-CH4drate.na))%>%
-  mutate(CH4drate.upper=24*exp(CH4drate.log+CH4drate.se))%>%
-  mutate(CH4drate.lower=24*exp(CH4drate.log-CH4drate.se))%>%
-  mutate(CH4erate.se=CH4erate.sd/sqrt(22-CH4erate.na))%>%
-  mutate(CH4erate.upper=24*exp(CH4erate.log+CH4erate.se))%>%
-  mutate(CH4erate.lower=24*exp(CH4erate.log-CH4erate.se))
+# Error_tributary<-summary_rate_error %>%
+#   filter(region=="tributary")%>%
+#   mutate(CH4trate.se=CH4trate.sd/sqrt(8-CH4trate.na))%>%
+#   mutate(CH4trate.upper=24*exp(CH4trate.se+CH4trate.log))%>%
+#   mutate(CH4trate.lower=24*exp(CH4trate.log-CH4trate.se))%>%
+#   mutate(CH4trate.logmean=24*exp(CH4trate.log))%>%
+#   mutate(meanCH4.funnel.se=meanCH4.funnel.sd/sqrt(8-meanCH4.funnel.na))%>%
+#   mutate(meanCH4.funnel.upper=24*exp(meanCH4.funnel.log+meanCH4.funnel.se-1))%>%
+#   mutate(meanCH4.funnel.lower=24*exp(meanCH4.funnel.log-meanCH4.funnel.se-1))
+#   mutate(CH4drate.se= CH4drate.sd/sqrt(8-CH4drate.na))%>%
+#   mutate(CH4drate.upper=24*exp(CH4drate.log+CH4drate.se))%>%
+#   mutate(CH4drate.lower=24*exp(CH4drate.log-CH4drate.se))%>%
+#   mutate(CH4erate.se=CH4erate.sd/sqrt(8-CH4erate.na))%>%
+#   mutate(CH4erate.upper=24*exp(CH4erate.log+CH4erate.se-1))%>%
+#   mutate(CH4erate.lower=24*exp(CH4erate.log-CH4erate.se-1))%>%
+#   mutate(CO2trate.se=CO2trate.sd/sqrt(8-CO2trate.na))%>%
+#   mutate(CO2trate.upper=24*exp(CO2trate.log+CO2trate.se-38))%>%
+#   mutate(CO2trate.lower=24*exp(CO2trate.log-CO2trate.se-38))
+# 
+# Error_main<-summary_rate_error %>%
+#   filter(region=="main")%>%
+#   mutate(CH4trate.se=CH4trate.sd/sqrt(22-CH4trate.na))%>%
+#   mutate(CH4trate.upper=24*exp(CH4trate.se+CH4trate.log))%>%
+#   mutate(CH4trate.lower=24*exp(CH4trate.log-CH4trate.se))%>%
+#   mutate(meanCH4.funnel.se=meanCH4.funnel.sd/sqrt(22-meanCH4.funnel.na))%>%
+#   mutate(meanCH4.funnel.upper=24*exp(meanCH4.funnel.log+meanCH4.funnel.se))%>%
+#   mutate(meanCH4.funnel.lower=24*exp(meanCH4.funnel.log-meanCH4.funnel.se))%>%
+#   mutate(CH4drate.se=CH4drate.sd/sqrt(22-CH4drate.na))%>%
+#   mutate(CH4drate.upper=24*exp(CH4drate.log+CH4drate.se))%>%
+#   mutate(CH4drate.lower=24*exp(CH4drate.log-CH4drate.se))%>%
+#   mutate(CH4erate.se=CH4erate.sd/sqrt(22-CH4erate.na))%>%
+#   mutate(CH4erate.upper=24*exp(CH4erate.log+CH4erate.se))%>%
+#   mutate(CH4erate.lower=24*exp(CH4erate.log-CH4erate.se))
---
+++ scriptsAndRmd/emission_scenarios.R
@@ -0,0 +1,53 @@
+#####################
+## this script estimates littoral tributary areas at different lake elevations
+## and then relates these estimates to estimates of total C flux and energy generation
+## started 5/19/2019
+#######################
+
+bathy<-read.csv(paste(myWD, "input/lp-predam-topo-multi-values-surf-area-vol-1128-to-1058m.csv", sep="/")) 
+head(bathy)
+
+bathy_by_elev<-bathy %>%
+  group_by(elevation) %>%
+  summarise(x2d_area=sum(X2d_area))
+
+elevations_m<-c(1128,1113,1098,1083,1109)
+#max capacities taken from spreadsheet that Lucas sent me
+maxcap_MWhperAF<-c(0.526276,0.481421,0.436567,0.391712,0.469521)
+#calculate max power generation based on 8.5 million acre feet
+MWh_y<-maxcap_MWhperAF*8500000
+#surface areas in acres are taken from Powe_ele-area_live.dat excel file from Heather Patno
+totalSA_acres<-c(161354,125504,96652,72997,117145)
+totalSA_km2<-totalSA_acres*0.00404686
+
+a<-bathy_by_elev[5,2]-bathy_by_elev[4,2]
+b<-bathy_by_elev[4,2]-bathy_by_elev[3,2]
+c<-bathy_by_elev[3,2]-bathy_by_elev[2,2]
+d<-bathy_by_elev[2,2]-bathy_by_elev[1,2]
+#from older spreadsheet that Tom Gushue sent 
+e<-33162391
+
+trib_lit_SA_m2<-c(42000761,33753536,27924903,18509832,33162391)
+trib_lit_SA_km2<-trib_lit_SA_m2/1000000
+
+upscaling<-cbind(trib_lit_SA_km2,totalSA_km2,MWh_y,elevations_m)
+upscaling<-data.frame(upscaling)
+upscaling$mainSA<-upscaling$totalSA_km2-upscaling$trib_lit_SA_km2
+upscaling$CH4kgyr<-upscaling$trib_lit_SA_km2*255.2*365+upscaling$mainSA*2.1*365
+upscaling$CO2kgyr<-upscaling$totalSA_km2*234*365
+upscaling$CO2ekgyr<-upscaling$CH4kgyr*34+upscaling$CO2kgyr
+upscaling$kgCO2eMWh<-upscaling$CO2ekgyr/upscaling$MWh_y
+upscaling$percent_littoral<-upscaling$trib_lit_SA_km2/upscaling$totalSA_km2*100
+
+
+upscaling_plot<-upscaling %>%
+  ggplot(aes(x=elevations_m,y=kgCO2eMWh))+
+  geom_point()+
+  theme_bw()
+upscaling_plot
+
+## need to add a point for the actual July data points
+
+
+
+
